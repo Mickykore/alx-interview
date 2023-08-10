06342ef9e08bb6b7152c64ac6bb5104c58e93701
mickykore <mikiyasayele6@gmail.com>
2023-08-10 20:50:39 +0300

    edit file 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,8 +3,7 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const filmEndPoint = url + movieId;
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 let people = [];
 const names = [];
 
